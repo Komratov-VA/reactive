2e14749615b608850aeb01235f21585265f019c7
komratov <17395719>
2020-04-30 20:31:58 +0300

    reactive webFluxExample

---
+++ src/main/java/com/example/reactive/domain/Message.java
@@ -0,0 +1,14 @@
+package com.example.reactive.domain;
+
+public class Message {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+   public Message(String data){
+        this.data = data;
+   }
+
+}
